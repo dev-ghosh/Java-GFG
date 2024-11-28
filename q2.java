b8ffe0158502730f9d20f103887cc8f8f01cba34
Dev Ghosh <dev.work616@gmail.com>
2024-11-29 02:36:01 +0530

    Add files via upload

---
+++ q2.java
@@ -0,0 +1,13 @@
+package GFG;
+import java.util.Scanner;
+public class q2 {
+    // to print @ (separator) between two numbers 
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String a =sc.nextLine();
+        String b=sc.nextLine();
+        String separator=sc.nextLine();
+        System.out.println(a+separator+b);
+    }
+
+}
